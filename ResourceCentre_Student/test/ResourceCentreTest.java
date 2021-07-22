6e27f1aec0a6430fbe72cc2ca87001f3787f0daa
20021437 <20021437@DESKTOP-36JAR11>
2021-07-22 12:52:43 +0800

    done testaddchromebook

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -52,6 +52,7 @@ public class ResourceCentreTest {
 	public void testAddChromebook() { //Brandon did this
 		//fail("Not yet implemented");
 		// write your code here
+		
 		// Item list is not null, so that can add a new item
 				assertNotNull("Test if there is valid Chromebook arraylist to add to", chromebookList);
 				
